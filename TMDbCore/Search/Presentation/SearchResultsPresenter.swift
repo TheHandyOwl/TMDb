744389f534416e83b0761f86edac66890393c74e
Carlos <thehandyowl.io@gmail.com>
2017-10-08 12:37:37 +0200

    Completamos la lógica de la caja y realiza las búsquedas con 2 o más caracteres buscados

--- TMDbCore/Search/Presentation/SearchResultsPresenter.swift
+++ TMDbCore/Search/Presentation/SearchResultsPresenter.swift
@@ -12,22 +12,32 @@ import RxSwift
 final class SearchResultsPresenter {
     
     private let detailNavigator: DetailNavigator
+    private let repository: SearchResultsRepositoryProtocol
 
 	/// The search query
 	let query = Variable("")
 
 	/// The search results
-	private(set) lazy var searchResults: Observable<[SearchResult]> = Observable
-		// TODO: implement
-		.just([.movie(Movie(identifier: 330459,
-		                   title: "Rogue One: A Star Wars Story",
-		                   posterPath: "/qjiskwlV1qQzRCjpV0cL9pEMF9a.jpg",
-		                   backdropPath: "/tZjVVIYXACV4IIIhXeIM59ytqwS.jpg",
-		                   releaseDate: "2016-11-02",
-		                   genreIdentifiers: [28, 12, 878]))])
+	//private(set) lazy var searchResults: Observable<[SearchResult]> = Observable
+    private(set) lazy var searchResults: Observable<[SearchResult]> = query.asObservable()
+        .distinctUntilChanged()
+        .debounce(0.3, scheduler: MainScheduler.instance)
+        .flatMapLatest { [weak self] query -> Observable<[SearchResult]> in
+            // Vamos a esperar a que el usuario teclee al menos 2 letras y devolveremos un array vacío si es menos
+            // Self no nulo y caracteres mayor que 2, si es menos entra en el guard / else
+            guard let `self` = self,
+                query.characters.count >= 2 else {
+                return Observable.just([])
+            }
+            
+            return self.repository.searchResults(withQuery: query, page: 1)
+        }
+        .share()
+        .observeOn(MainScheduler.instance)
 
-    init(detailNavigator: DetailNavigator) {
+    init(detailNavigator: DetailNavigator, repository: SearchResultsRepositoryProtocol) {
         self.detailNavigator = detailNavigator
+        self.repository = repository
     }
     
 	/// Called by the view when the user selects a search result
