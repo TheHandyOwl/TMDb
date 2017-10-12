61789600d26a49575ac78f5d9ffa0c6b5cbbe23c
Carlos <thehandyowl.io@gmail.com>
2017-10-12 15:15:51 +0200

    Tests de navegación

---
+++ TMDbCoreTests/FeaturedRepositoryMock.swift
@@ -0,0 +1,32 @@
+//
+//  FeaturedRepositoryMock.swift
+//  TMDbCoreTests
+//
+//  Created by Carlos on 12/10/17.
+//  Copyright © 2017 Guille Gonzalez. All rights reserved.
+//
+
+@testable import TMDbCore
+import RxSwift
+
+final class FeaturedRepositoryMock: FeaturedRepositoryProtocol {
+    private(set) var moviesNowPlayingCalled = false
+    private(set) var moviesNowPlayingParameters: String?
+    var moviesNowPlayingResult = Observable<[Movie]>.just([])
+    
+    private(set) var showsOnTheAirCalled = false
+    var showsOnTheAirResult = Observable<[Show]>.just([])
+    
+    func moviesNowPlaying(region: String) -> Observable<[Movie]> {
+        moviesNowPlayingCalled = true
+        moviesNowPlayingParameters = region
+        
+        return moviesNowPlayingResult
+    }
+    
+    func showsOnTheAir() -> Observable<[Show]> {
+        showsOnTheAirCalled = true
+        return showsOnTheAirResult
+    }
+}
+
